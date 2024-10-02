d67eb76af455bad17a85aa2e4ce1b3dcc8b43bfe
basdijkstra <bas@ontestautomation.com>
2024-10-02 12:20:57 +0200

    Dit is mijn commit-message

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -36,4 +36,4 @@ def test_overdraw_checking():
     account.withdraw(100)
     assert account.balance == -50
 
-# TODO: die test moet wel een keer gefixt worden
+# TODO: die test moet wel een keer gefixt worden, dus schiet een beetje op.
